56b4c7208300d4d5bcf445907cb0ffb1a4ed5abc
Reza <sadegh_152@yahoo.com>
2013-11-14 06:00:21 +0330

    OK

---
+++ admin/core/up_exams.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once(dirname(__FILE__)."/../include.php");
+$db = DBSingleton::getInstance();
+
+$conds = " start_date>=now() ";
+$tody_exams= $db->dbSelect("exams", $conds);
+foreach($tody_exams as $k => $ex){
+    $ret = $db->dbSelect("questions", "id in (select qid from exam_qs where eid={$ex['id']})", "", 0, -1, array("sum(mark) as mark"));
+    $tody_exams[$k]["marks"] = (int)$ret[0]["mark"];
+    
+    $ret = $db->dbSelect("exam_stdgroups join stdgroups on(gid=stdgroups.id)", "eid={$ex['id']}");
+    $gs = array();
+    foreach($ret as $g){
+        $gs[] = $g["title"];
+    }
+    $tody_exams[$k]["groups"] = implode(" | ", $gs);
+}
+
+$cols = array("start_date","name", "groups", "marks", "duration");
+$tody_exams = arraySelectKeys($tody_exams, $cols);
+
+
+header('Content-Type: application/json');
+$ret = arrayPHPToJS($tody_exams);
+echo '{"sEcho":"2","iTotalRecords":'.count($tody_exams).',"iTotalDisplayRecords":'.count($tody_exams).',"aaData":  '.$ret.'}';
+?>
--- admin/home.php
+++ admin/home.php
@@ -1,21 +1,67 @@
 <?php include("header.php");?>
 
+
+<script type="text/javascript" charset="utf-8">    
     
-    <script type="text/javascript">
-
-    jQuery(document).ready(function(){
-        $('#home_li').addClass('active');
-    });
+    $(document).ready(function() {
     
-    </script>
+                                $('#up_exams_table').dataTable({
+                                
+                                    "bPaginate": false,  
+                                    "bInfo": false,  
+                                    "bFilter": false,
+                                    "bAutoWidth": false,
+				    "sAjaxSource": "core/up_exams.php",
+                                    "bProcessing": true,
+                                });
+                                
+                                $('#rec_exams_table').dataTable({
+                                
+                                    "bPaginate": false,  
+                                    "bInfo": false,  
+                                    "bFilter": false,
+                                    "bAutoWidth": false,
+				    "sAjaxSource": "core/up_exams.php",
+                                    "bProcessing": true,
+                                });
+                                
     
-
-    <div class="container">
-
-      <div class="starter-template">
-        <h1>Here is admin Homepage/Dashboard</h1>
-      </div>
-
-    </div><!-- /.container -->
+			});
+</script>
+    
+    
+    
+    <div id="page-content-wrapper">
+        <div class="content-header">
+          <h1>
+            <!--a class="btn btn-default" href="#" id="menu-toggle"><i class="icon-reorder"></i></a-->
+          </h1>
+        </div>
+        <!-- Keep all page content within the page-content inset div! -->
+        <div class="page-content inset">
+          
+                    <div class="row">
+                        <div class="col-md-8">
+                            <h4>Upcoming exams</h4>
+                          <center>
+                            <table cellpadding="0" cellspacing="0" border="0" class="table table-striped table-bordered" id="up_exams_table" style="width:800px" >
+	                        <thead>
+		                    <tr>
+			            <th>Date</th>
+			            <th>Exam Name</th>
+			            <th>Group</th>
+			            <th>Marks</th>
+			            <th>Duration</th>
+			
+		                    </tr>
+	                        </thead>
+                            </table>
+                            </center>
+                    </div>
+                    </div>
+          </div>
+        </div>
+    
+    
 
 <?php include("footer.php");?>
